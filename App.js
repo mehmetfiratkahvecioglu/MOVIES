883870823d4599b398a29af69f974e0e9d2eab7d
Fırat <mehmetfirat0707@hotmail.com>
2022-11-26 22:23:43 +0300

    finished vector-icons setup

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Icon name="rocket" size={30} color="#900" />
+    </View>
+  );
+};
+
+export default App;
