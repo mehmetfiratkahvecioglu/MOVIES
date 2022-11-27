d8ee537deb32230867e4cb93976aa978e5d34feb
Fırat <mehmetfirat0707@hotmail.com>
2022-11-27 15:42:14 +0300

    first slider implemented

--- src/components/DiscoverMovies/DiscoverMovies.js
+++ src/components/DiscoverMovies/DiscoverMovies.js
@@ -1,12 +1,26 @@
 import {View, Text} from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {GET} from '../../services/API';
-
+import {BASE_IMAGE_POSTER_URL} from '../../config';
+import {SliderBox} from 'react-native-image-slider-box';
 const DiscoverMovies = () => {
+  const [movies, setMovies] = useState([]);
+  const [images, setImages] = useState([]);
+
   useEffect(() => {
     const getMovies = async () => {
       let response = await GET('/discover/movie');
-      console.log(response);
+      setMovies(response.results);
+
+      let apiImages = await response.results.map(
+        data => `${BASE_IMAGE_POSTER_URL}${data.backdrop_path}`,
+      );
+
+      let PosterImagesForSlider = [];
+      for (let i = 0; i < 10; i++) {
+        PosterImagesForSlider = [...PosterImagesForSlider, apiImages[i]];
+      }
+      setImages(PosterImagesForSlider);
     };
     getMovies();
   }, []);
@@ -14,6 +28,7 @@ const DiscoverMovies = () => {
   return (
     <View style={{backgroundColor: 'red'}}>
       <Text>DiscoverMovies</Text>
+      <SliderBox images={images} />
     </View>
   );
 };
