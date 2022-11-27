a0c58504b8733256c9cae7e2f82631727a2e0433
Fırat <mehmetfirat0707@hotmail.com>
2022-11-27 13:25:07 +0300

    react-navigation header design

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,33 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
+import HomeScreen from './screens/HomeScreen';
+import Constants from './Constants';
+import Icon from 'react-native-vector-icons/Ionicons';
 const Stack = createNativeStackNavigator();
 
 function Router() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'MOVIES',
+            headerTitleAlign: 'center',
+            headerStyle: {backgroundColor: Constants.baseColor},
+            headerTitleStyle: {
+              color: Constants.textColor,
+            },
+            headerLeft: () => (
+              <Icon name="menu-sharp" size={30} color={Constants.textColor} />
+            ),
+            headerRight: () => (
+              <Icon name="search-sharp" size={30} color={Constants.textColor} />
+            ),
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {SliderBox} from 'react-native-image-slider-box';
 
-const App = () => {
+const HomeScreen = () => {
   const images = [
     'https://images.unsplash.com/photo-1591448764624-d7973a442bff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80',
     'https://images.unsplash.com/photo-1602018979916-2dc45cb2e10c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
@@ -20,4 +20,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default HomeScreen;
